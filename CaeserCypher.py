c1424a5385520ca128e58e6be5558d784bc564aa
Michael Allport <ma18533@essex.ac.uk>
2021-05-18 01:28:07 +0100

    slightly refactored, included encode and decode method instead of using boolean flag

--- CaeserCypher.py
+++ CaeserCypher.py
@@ -1,40 +1,49 @@
-class CaeserCypher():
+class CaesarCypher():
     def __init__(self, charShift: int):
         if charShift > 26 or charShift < 0:
-            raise Exception("Invalid caeser shit exceeding alphabet or lower than 0")
-        self.__CHAR_SHIFT = charShift
+            raise Exception("Invalid caesar shit exceeding alphabet or lower than 0")
+        self.charShift = charShift
+        self.caesarMessage = ""
         self.__Z = 122
         self.__A = 97
         
-    def getCaeserFromString(self, inputStr: str, toEncode: bool):
-        caeserString = ""
-        for character in inputStr:
-            caeserString = caeserString + self.__getCaeserConverterAscii(character, toEncode)
-        return caeserString
+    def encodeString(self, stringToEncode: str):
+        self.caesarMessage = stringToEncode
+        self.__setCaesarMessage()
+    
+    def decodeString(self, stringToDecode: str):
+        self.caesarMessage = stringToDecode
+        self.__invertCharShift()
+        self.__setCaesarMessage()
+        self.__invertCharShift()
+        
+    def __invertCharShift(self):
+        self.charShift = -self.charShift
+        
+    def __setCaesarMessage(self):
+        caesarString = ""
+        for character in self.caesarMessage:
+            caesarString = caesarString + self.__getCaesarConvertedAscii(character)
+        self.caesarMessage = caesarString
                 
-    def __getCaeserConverterAscii(self, inputCharacter: str, toEncode: bool):
-        # are we encoding or decoding
-        if toEncode:
-            newAsciiValue = ord(inputCharacter) + self.__CHAR_SHIFT
-        else:
-            newAsciiValue = ord(inputCharacter) - self.__CHAR_SHIFT
-            
-        # handle if encoded > z
+    def __getCaesarConvertedAscii(self, inputCharacter: str):
+        newAsciiValue = ord(inputCharacter) + self.charShift
+        
         howManyGreaterThanZ = self.__asciiValueAboveZ(newAsciiValue)
         if howManyGreaterThanZ > 0:
             return chr(self.__A - 1 + howManyGreaterThanZ)
         
-        # handle if < a
         howManyLessThanA = self.__asciiValueBelowA(newAsciiValue)
         if howManyLessThanA > 0:
             return chr(self.__Z + 1 - howManyLessThanA)
 
-        # its fine
         return chr(newAsciiValue)
-            
     
     def __asciiValueAboveZ(self, asciiInput: int):
         return asciiInput - self.__Z
     
     def __asciiValueBelowA(self, asciiInput: int):
         return self.__A - asciiInput
+    
+    def getCaesarMessage(self):
+        return self.caesarMessage
--- README.md
+++ README.md
@@ -4,4 +4,9 @@ Have tried using CleanCode practice with naming conventions, magic numbers etc, 
 
 ## Usage  
 Create a CaeserCypher instance passing the shift value as argument.  
-Method 'getCaeserFromString' takes the input string, and a boolean 'toEncode' indicating whether this is to encoded or decoded. This returns the encoded or decoded string
+Method 'encodeString' takes the input string and sets member variable caesarMessage to encoded message  
+Method 'decodeString' takes the input string and decodes same as above  
+Method 'getCaesarMessage' returns the encoded/decoded message  
+  
+## Things to keep in mind  
+This assumes every input character is is within the alphabet range of 'a' - 'z', given quick google of the definition states that was the intent of the caeser cypher, though some places used captilized alphabet some lowercase. If using this you should add error checking in the main getCaeserFromString method during the character loop  
